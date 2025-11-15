3c14f3791d424a71a160041a079a8bd233a36c1a
Ruman Akhtar <cseruman42@gmail.com>
2025-11-15 23:14:12 +0530

    utils updated

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility: cn()
+ * ----------------------------------
+ * Merges class names using clsx + tailwind-merge.
+ * Prevents conflicting Tailwind classes and keeps final output clean.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
